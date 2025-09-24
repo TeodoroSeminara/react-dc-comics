abc4dfb7d427aad756eb46f090fe1d9acd5dbedb
Teo <seminara.teodoro93@gmail.com>
2025-09-24 23:24:25 +0200

    header ok

--- src/components/MySocial.jsx
+++ src/components/MySocial.jsx
@@ -2,7 +2,17 @@ const MySocial = () => {
     return (
         <div className="social-container">
             <div className="social container">
-
+                <a href="@" className="sign-up">SIGN-UP NOW!</a>
+                <div className="follow">
+                    <span>FOLLOW US</span>
+                    <div className="img-social">
+                        <a href="#"><img src="../src/assets/img/footer-facebook.png" alt="" /></a>
+                        <a href="#"><img src="../src/assets/img/footer-twitter.png" alt="" /></a>
+                        <a href="#"><img src="../src/assets/img/footer-youtube.png" alt="" /></a>
+                        <a href="#"><img src="../src/assets/img/footer-pinterest.png" alt="" /></a>
+                        <a href="#"><img src="../src/assets/img/footer-periscope.png" alt="" /></a>
+                    </div>
+                </div>
             </div>
         </div>
     )
