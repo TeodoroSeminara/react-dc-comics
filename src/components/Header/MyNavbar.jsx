6a26d01278cd258381efef465ba6fc9b18684625
Teo <seminara.teodoro93@gmail.com>
2025-09-26 00:49:58 +0200

    navbar refactoring

--- src/components/Header/MyNavbar.jsx
+++ src/components/Header/MyNavbar.jsx
@@ -1,10 +1,21 @@
 const MyNavbar = () => {
 
-    const navbarLinks = ["CHARACTERS", "COMICS", "MOVIES", "TV", "GAMES", "COLLECTIBLES", "VIDEOS", "FANS", "NEWS", "SHOP"];
+    const navbarLinks = [
+        { id: 1, url: "#", name: "CHARACTERS", class: false },
+        { id: 2, url: "#", name: "COMICS", class: false },
+        { id: 3, url: "#", name: "MOVIES", class: false },
+        { id: 4, url: "#", name: "TV", class: false },
+        { id: 5, url: "#", name: "GAMES", class: false },
+        { id: 6, url: "#", name: "COLLECTIBLES", class: false },
+        { id: 7, url: "#", name: "VIDEOS", class: false },
+        { id: 8, url: "#", name: "FANS", class: false },
+        { id: 9, url: "#", name: "NEWS", class: false },
+        { id: 10, url: "#", name: "SHOP", class: false }
+    ];
 
     const linkNav = navbarLinks.map(navbarLink => (
         <li>
-            <a href="#">{navbarLink}</a>
+            <a key={navbarLink.id} href={navbarLink.url}>{navbarLink.name}</a>
         </li>
     ));
 
